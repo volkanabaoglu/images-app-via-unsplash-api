cbbaf3171b091c9faece5d9d7e1e081d38fbe886
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-01 18:14:08 +0300

    Last update

--- src/api.js
+++ src/api.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import 'dotenv/config'
+import "dotenv/config";
+import process from 'process'
+
+const secret_key = process.env.SECRET_KEY;
+const api_url = process.env.API_URL;
 const searchImages = async (term) => {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
+  const response = await axios.get(api_url, {
     headers: {
-      Authorization:process.SECRET_KEY,
+      Authorization: secret_key,
     },
     params: {
       query: term,
     },
   });
-  
+
   return response.data.results;
 };
 
